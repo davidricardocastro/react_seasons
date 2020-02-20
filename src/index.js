be80503ef8a2ff5dab8b976c1ad8bf1a601bb62e
David Castro <davidricardocastro@outlook.com>
2020-02-20 18:22:39 +0200

    initial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* Functional component
+const App = () =>{
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
+return <div>Latitude: </div>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+};
+*/
+
+//Class based component
+class App extends React.Component {
+
+    /*
+    //With constructor method
+    constructor(props) {
+        super(props);
+        this.state = { lat: null, errorMessage: '' };        
+    }
+    */
+
+    //Works the same as above
+    state = { lat: null, errorMessage: '' };
+
+    componentDidMount() {
+        console.log('My component was rendered to the screen');
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+
+    componentDidUpdate() {
+        console.log('My component was just updated - it re-rendered!!');
+    }
+
+    //helper function
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>;
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+
+        return <Spinner message="Please accept location request" />;
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
